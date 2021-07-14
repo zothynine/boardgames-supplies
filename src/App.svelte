9a1ce97cde0b3a777d8c3b98bdd769377a804aa9
Mario Zoth <mario.zoth@icloud.com>
2021-07-14 22:02:49 +0200

    Four empty cards with snap scrolling

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,22 @@
 <script>
-	export let name;
+	import Card from './Card.svelte'
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Card color="blue"/>
+	<Card color="red"/>
+	<Card color="yellow"/>
+	<Card color="green"/>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		display: flex;
+		position: absolute;
+		inset: 0;
+		overflow-x: auto;
+		overflow-y: hidden;
+    	scroll-snap-type: x mandatory;
+		height: 100vh;
 	}
 </style>
---
+++ src/Card.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let color = "white";
+</script>
+
+<section>
+    <div class="card" style="--card-color: {color}">
+        <span>{color}</span>
+    </div>
+</section>
+
+<style>
+    section {
+        align-items: center;
+        display: flex;
+        flex: 0 0 100vw;
+        justify-content: space-around;
+        margin: 0;
+        padding: 0;
+        scroll-snap-align: start;
+    }
+
+    .card {
+        background-color: var(--card-color);
+        border-radius: 16px;
+        box-sizing: border-box;
+        height: min(130vw, 90vh);
+        padding: 16px;
+        width: clamp(320px, 60vh, 90vw);
+    }
+</style>
