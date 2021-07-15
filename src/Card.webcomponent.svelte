cf7a75723ed1a03188dee0e27da70076460ba4f6
Mario Zoth <mario.zoth@icloud.com>
2021-07-15 20:25:38 +0200

    Add branding

--- src/Card.webcomponent.svelte
+++ src/Card.webcomponent.svelte
@@ -5,7 +5,10 @@
 </script>
 
 <div class="card" style="--card-color: {color}">
-    <span>{color}</span>
+    <span class="branding">
+        <strong>Uno</strong>
+        Würfelspiel
+    </span>
 </div>
 
 <style>
@@ -16,5 +19,20 @@
         height: min(130vw, 90vh);
         padding: 16px;
         width: clamp(320px, 60vh, 90vw);
+    }
+
+    .branding {
+        color: white;
+        display: inline-block;
+        font-weight: bold;
+        text-align: center;
+        text-shadow: -2px 0 0 black;
+        transform: rotate(-8deg);
+    }
+
+    .branding strong {
+        display: block;
+        font-size: 2rem;
+        text-shadow: -4px 0 0 black;
     }
 </style>
