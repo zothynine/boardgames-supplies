41da469e1358875163652cc0ec86aab9b54c234d
Mario Zoth <mario.zoth@icloud.com>
2022-01-17 16:59:35 +0000

    Add DKT button to menu

--- src/Menu.svelte
+++ src/Menu.svelte
@@ -1,10 +1,12 @@
 <script>
   import { app as storedApp } from "./stores";
   import UnoDiceGame from "./unodicegame/UnoDiceGame.svelte";
+  import Dkt from "./dkt/Dkt.svelte";
 </script>
 
 <main>
   <button on:click={ () => storedApp.set(UnoDiceGame) }>Uno Dice Game</button>
+  <button on:click={ () => storedApp.set(Dkt) }>DKT</button>
   <!-- <button on:click={ () => storedApp.set(UnoDiceGame) }>Whatever Comes Next</button> -->
 </main>
 
