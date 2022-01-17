b665035d1c731402195cbc1cf8a1f1182a30dc69
Mario Zoth <mario.zoth@icloud.com>
2022-01-17 16:54:27 +0000

    Fix layout

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   import Menu from "./Menu.svelte";
   import { app as storedApp } from "./stores";
 
-  const VERSION = "1.0.10";
+  const VERSION = "1.0.11";
   $: app = Menu;
   storedApp.subscribe(value => app = value);
 </script>
@@ -15,17 +15,26 @@
 <em class="version">{VERSION}</em>
 
 <style global>
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+    margin: 0;
+  }
+
   .back {
     position: fixed;
-    inset: 0.25rem 0 0 0.25rem;
+    top: 0.25rem;
+    right: 0.25rem;
     font-size: 0.75rem;
     line-height: 1rem;
     opacity: 0.5;
+    background: transparent;
+    border-width: 0;
   }
 
   .version {
       position: fixed;
-      inset: 0.25rem 0.25rem 0 0;
+      top: 0.25rem;
+      left: 0.25rem;
       color: black;
       font-size: 0.75rem;
       line-height: 1rem;
